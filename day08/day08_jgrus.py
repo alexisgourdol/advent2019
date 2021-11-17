24db6d2fa60d0c54c2be99713bbf647ab30afad2
Alexis Gourdol <alexis.gourdol@gmail.com>
2021-11-17 20:44:53 +0000

    day08 part 2 from jgrus

--- day08/day08_jgrus.py
+++ day08/day08_jgrus.py
@@ -1,6 +1,7 @@
 from typing import Dict
 from typing import List
 from collections import Counter
+import copy
 
 Image = List[List[List[int]]]
 
@@ -46,12 +47,37 @@ def one_times_two(image: Image) -> int:
     return min_layer_counter[1] * min_layer_counter[2]
 
 
+def show(image: Image) -> None:
+    consolidated = copy.deepcopy(image[0])
+    num_layers = len(image)
+    height = len(image[0])
+    width = len(image[0][0])
+
+    for i in range(height):
+        for j in range(width):
+            for layer in range(num_layers):
+                color = image[layer][i][j]
+                if color == 0:
+                    consolidated[i][j] = " "
+                    break
+                if color == 1:
+                    consolidated[i][j] = "*"
+                    break
+    for row in consolidated:
+        print("".join(row))
+
+
 RAW = "123456789012"
 IMAGE = parse_image(RAW, 3, 2)
 assert IMAGE == [[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [0, 1, 2]]]
 assert one_times_two(IMAGE) == 1
 
+RAW_2 = "0222112222120000"
+IMAGE_2 = parse_image(RAW_2, 2, 2)
+show(IMAGE_2)
+
 with open("day08.txt") as f:
     inp = f.readline().strip()
     image = parse_image(inp, 25, 6)
     print(f"{one_times_two(image)=}")
+    show(image)
